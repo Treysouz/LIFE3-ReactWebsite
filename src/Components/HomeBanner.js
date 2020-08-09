4b75b2a136c697842c0f18b892c0cf0ab3310413
Tremayne Souza <treysouz@gmail.com>
2020-08-09 10:35:08 -0400

    Added navbar and home page banner

---
+++ src/Components/HomeBanner.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class HomeBanner extends Component {
+  render() {
+    return (
+      <div className="homeBanner">
+        <span className="navClickArea" ></span>
+        <span className="navClickArea" ></span>
+        <span className="navClickArea" ></span>
+      </div>
+    );
+  }
+}
---
+++ src/Pages/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import HomeBanner from "../Components/HomeBanner";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <section>
+        <HomeBanner></HomeBanner>
+      </section>
+    );
+  }
+}
