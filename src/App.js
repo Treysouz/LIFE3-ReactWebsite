28b3157c4467b5eb6be23b517b5f87feac47a062
Sambeg Raj Subedi <subedisambeg111@gmail.com>
2020-11-20 08:58:44 -0500

    google analytics

--- src/App.js
+++ src/App.js
@@ -6,6 +6,44 @@ import Home from "./Pages/Home";
 import Empower from "./Pages/Empower";
 import Banner from "./Components/Banner";
 import Footer from "./Components/Footer";
+// for google analytics 
+
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-179955369-1');
+ReactGA.pageview('/about/contact-us', ['tracker2']);
+
+ReactGA.initialize('UA-000000-01', {
+  debug: true,
+  titleCase: false,
+  gaOptions: {
+    userId: 123
+  }
+});
+
+ReactGA.addTrackers(
+  [
+    {
+      trackingId: 'UA-000000-01',
+      gaOptions: {
+        name: 'tracker1',
+        userId: 123
+      }
+    },
+    {
+      trackingId: 'UA-000000-02',
+      gaOptions: { name: 'tracker2' }
+    }
+  ],
+  { debug: true, alwaysSendToDefaultTracker: false }
+);
+////// end   
+
+export const PageView = () => {  
+  ReactGA.pageview(window.location.pathname +  
+                   window.location.search); 
+}
+
 
 export default class App extends Component {
   constructor() {
